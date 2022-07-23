9e50deb78b93551b6362b10a0a6207e958a00eec
Laboratório 03 - FACEC <facec.lab03@faculdadefacec.intranet>
2022-07-23 16:39:27 -0300

    Primeiro commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Repositório de teste para código JavaScript
+
+Para executar o código digite no terminal:
+
+```
+node example.js
+```
---
+++ example.js
@@ -0,0 +1,5 @@
+let nome = 'Carlos'; //let= Permite alterar a variavel//
+console.log(`Olá, ${nome}! Seja bem-vindo!`)
+
+nome = 'Eduardo'; 
+console.log(`Olá, ${nome}! Seja bem-vindo!`)
